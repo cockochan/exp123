4aa634a7622ebef8bf5de5639f9dc6b6128db4ab
Constantin Dimitrenco <56898295+cockochan@users.noreply.github.com>
2023-05-08 15:12:41 +0100

    ok

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+
 const dev = process.env.NODE_ENV !== "production";
 const path = require("path");
 const express = require("express");
@@ -9,11 +10,20 @@ if (dev) {
   const webpackDev = require("./dev");
   app.use(webpackDev.comp).use(webpackDev.hot);
 }
+const swaggerUi = require("swagger-ui-express");
+swaggerDocument = require("./swagger.json");
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
-
+app.get("/services", (req, res) => {
+  const { name } = req.params;
+  res.json({
+    message: `ok services will be here!`,
+  });
+});
 app.listen(3000, function () {
   console.log("Server started on :3000");
 });
